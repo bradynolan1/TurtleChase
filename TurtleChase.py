e4f111df788995b4ba067f5871e41511dcf952d2
Brady Nolan <nolanb@gilmour.org>
2019-09-20 09:39:58 -0400

    b dolla

--- TurtleChase.py
+++ TurtleChase.py
@@ -8,9 +8,44 @@ wn.bgcolor("pink")
 player_one = superturtle.SuperTurtle()
 player_two = superturtle.SuperTurtle()
 
+player_one.write("Bet you can't catch me!", font=('Arial', 12, 'bold'))
+
 # helper functions
 def quit_window():
    wn.bye() 
+
+def check():
+   print("checking")
+   keep_on_screen()
+   collison()
+   wn.ontimer(check,10)
+
+def keep_on_screen():
+   #check player_one on all four angles
+   if player_one.xcor() <-250:
+      player_one.goto(-249,player_one.ycor())
+   if player_one.xcor() >250:
+      player_one.goto(249,player_one.ycor())
+   if player_one.ycor() > 250:
+      player_one.goto(player_one.xcor(),249)
+   if player_one.ycor() < -250:
+      player_one.goto(player_one.xcor(),-249)
+   #check player_two on all four angles
+   if player_two.xcor() <-250:
+      player_two.goto(-249,player_two.ycor())
+   if player_two.xcor() >250:
+      player_two.goto(249,player_two.ycor())
+   if player_two.ycor() > 250:
+      player_two.goto(player_two.xcor(),249)
+   if player_two.ycor() < -250:
+      player_two.goto(player_two.xcor(),-249)
+  
+def collison():
+   x_diff = abs(player_one.xcor() - player_two.xcor())
+   y_diff = abs(player_one.ycor() - player_two.ycor())
+   if x_diff < 20 and y_diff < 20:
+      quit_window()
+
 
 # PLAYER ONE CONTROLS
 wn.onkey(player_one.move_forward, "Up")
@@ -26,4 +61,5 @@ wn.onkey(player_two.turn_right, "d")
 wn.onkey(quit_window, "q")
 
 wn.listen()
+check()
 wn.mainloop()
